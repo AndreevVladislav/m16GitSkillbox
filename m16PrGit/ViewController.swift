c475d2830732350bbe3a6368ca567c58687029ea
VladislavAndreev <vlad070701.vlad@yandex.ru>
2023-04-06 17:56:40 +0300

    Initial Commit

---
+++ m16PrGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16PrGit
+//
+//  Created by Vladislav Andreev on 06.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
